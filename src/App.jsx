d4dd8ffa860da5b2f65ae40c4af5e354fa050f93
Rayalj <mrayalejandro720@gmail.com>
2024-04-16 20:54:00 -0700

    lesson_1_7

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,29 @@ import React, { Fragment, useEffect, useState } from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
-function useSemiPersistentState(key, initialState) {
-  const [value, setValue] = useState(
-    JSON.parse(localStorage.getItem(key)) || initialState
-  );
+function App() {
+  const key = 'savedTodoList';
+  const initialState = JSON.parse(localStorage.getItem(key)) || [];
+  const [todoList, setTodoList] = useState(initialState);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
-  }, [key, value]);
+    const fetchData = async () => {
+      try {
+        // Simular una llamada a una API o un proceso de carga de datos
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        
+        // Una vez que los datos se cargan con éxito, actualizamos el estado
+        setTodoList(initialState);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error al cargar los datos:', error);
+        setIsLoading(false); // En caso de error, también detenemos el indicador de carga
+      }
+    };
 
-  return [value, setValue];
-}
-
-function App() {
-  const [todoList, setTodoList] = useSemiPersistentState('savedTodoList', []);
+    fetchData();
+  }, [initialState]);
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
@@ -27,15 +36,17 @@ function App() {
   };
 
   useEffect(() => {
-    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-  }, [todoList]);
+    if (!isLoading) {
+      localStorage.setItem(key, JSON.stringify(todoList));
+    }
+  }, [todoList, isLoading]);
 
   return (
     <Fragment>
-      <h1>To Do List</h1>
+      <h1>TO DO LIST </h1>
       <AddTodoForm addTodo={addTodo} />
       <hr />
-      <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      {isLoading ? <p>Cargando...</p> : <TodoList todoList={todoList} onRemoveTodo={removeTodo} />}
     </Fragment>
   );
 }
